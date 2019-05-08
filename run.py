4712f798e12b8b94a42108d83c527c1ebfebaca0
santiago <santiago30mw@gmail.com>
2019-05-08 11:26:07 +0100

    v1.0

---
+++ run.py
@@ -0,0 +1,18 @@
+# Search methods
+
+import search
+
+ab = search.GPSProblem('A', 'B'
+                       , search.romania)
+
+print(search.breadth_first_graph_search(ab).path())
+print(search.depth_first_graph_search(ab).path())
+print(search.branch_boundary_graph_search(ab).path())
+print(search.branch_boundary_with_underestimation_graph_search(ab).path())
+
+# Result:
+# [<Node B>, <Node F>, <Node S>, <Node A>] : 211 + 99 + 140 = 450
+# [<Node B>, <Node P>, <Node C>, <Node D>, <Node M>, <Node L>, <Node T>, <Node A>] : 101+138+120+75+70+111+118 = 733
+# [<Node B>, <Node P>, <Node R>, <Node S>, <Node A>] : 101 + 97 + 80 + 140 = 418
+
+
